4ac8368e0c61061d954eb5964cb1bd7561776ebf
glitchX-Harshit <hc5764802@gmail.com>
2025-12-19 20:50:50 +0530

    tech stack updated

--- iceSlide/data.js
+++ iceSlide/data.js
@@ -60,45 +60,57 @@ export const awards = [
         label: "Animation Excellence"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
     },
     {
-        name: "Best Animation Award",
-        type: "Gold",
-        project: "INNOVATE 2023",
-        label: "Animation Excellence"
-    }
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
+    },
+    {
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
+    },
+    {
+        name: "Rapid React Development",
+        type: "RANK- 3",
+        project: "INFINITO 2025",
+        label: "Web Development webrunz"
+    },
 ]
